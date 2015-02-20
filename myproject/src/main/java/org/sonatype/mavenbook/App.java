ffbd44e6d1775167105900d0a18e6d74656f61e8
Ehsan Haider <ehsan@Ehsans-MacBook-Pro.local>
2015-02-20 17:42:10 -0600

    added new tag

--- myproject/src/main/java/org/sonatype/mavenbook/App.java
+++ myproject/src/main/java/org/sonatype/mavenbook/App.java
@@ -3,7 +3,7 @@ package org.sonatype.mavenbook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//tag v1.1
+//tag v1.2
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
