dd783d617c8acbbbe4e29d2a9741650b8e4556d7
Ehsan Haider <ehsan@Ehsans-MacBook-Pro.local>
2015-02-20 17:36:33 -0600

    tag test

--- myproject/src/main/java/org/sonatype/mavenbook/App.java
+++ myproject/src/main/java/org/sonatype/mavenbook/App.java
@@ -3,6 +3,7 @@ package org.sonatype.mavenbook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//tag v1.1
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
